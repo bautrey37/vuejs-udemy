eb97928b94e2c83b80897bb04e350d1cea548f5a
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-05-02 20:49:02 +0300

    moving stuff around

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vuejs2-udemy
+
 https://www.udemy.com/course/vuejs-2-the-complete-guide/
+
+## Section 2
+
+Copy to JSFiddle
--- section_2/app.js
+++ section_2/app.js
@@ -2,14 +2,18 @@ new Vue({
 	el: '#app',
 	data: {
 		title: 'Hello World!',
-    world: 'World!',
-    link: 'http://google.com',
-    finishedLink: '<a href="http://google.com">Google</a>'
+		counter: 0,
+		world: 'World!',
+		link: 'http://google.com',
+		finishedLink: '<a href="http://google.com">Google</a>',
 	},
 	methods: {
-		sayHello: function() {
-    	this.title = 'Hello!!'
+		sayHello: function () {
+			this.title = 'Hello!!';
 			return 'Hello ' + this.world;
-		}
-	}
-})
+		},
+		increase: function () {
+			this.counter++;
+		},
+	},
+});
