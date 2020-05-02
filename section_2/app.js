78f09860023e54fac7187e8f05067fc7ec7c43e2
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-05-02 21:07:10 +0300

    events and key listeners

--- section_2/app.js
+++ section_2/app.js
@@ -2,18 +2,30 @@ new Vue({
 	el: '#app',
 	data: {
 		title: 'Hello World!',
-		counter: 0,
 		world: 'World!',
 		link: 'http://google.com',
 		finishedLink: '<a href="http://google.com">Google</a>',
+		counter: 0,
+		x: 0,
+		y: 0,
 	},
 	methods: {
 		sayHello: function () {
 			this.title = 'Hello!!';
 			return 'Hello ' + this.world;
 		},
-		increase: function () {
-			this.counter++;
+		increase: function (step, event) {
+			this.counter += step;
 		},
+		updateCoordinates: function (event) {
+			this.x = event.clientX;
+			this.y = event.clientY;
+		},
+    dummy: function(event) {
+    	event.stopPropagation();
+    },
+    alertMe: function() {
+    	alert('Alert');
+    }
 	},
 });
