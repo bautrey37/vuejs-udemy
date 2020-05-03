bccc6f76d4a7377faaaf2dac5201d072690f6cc1
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-05-03 12:38:30 +0300

    exercise 4

--- Assignments/Exercise 4/app.js
+++ Assignments/Exercise 4/app.js
@@ -1,11 +1,40 @@
 new Vue({
-  el: '#exercise',
-  data: {
-
-  },
-  methods: {
-    startEffect: function() {
-    
-    }
-  }
+	el: '#exercise',
+	data: {
+		effectClasses: {
+			highlight: false,
+			shrink: true,
+		},
+		float: 'float',
+		customClass: '',
+		isVisible: true,
+		myStyle: {
+			width: '100px',
+			height: '150px',
+			backgroundColor: 'gray',
+		},
+		progressBar: {
+			height: '20px',
+			backgroundColor: 'blue',
+			width: '0px',
+		},
+	},
+	methods: {
+		startEffect: function () {
+			var vm = this;
+			setInterval(function () {
+				vm.effectClasses.highlight = !vm.effectClasses.highlight;
+				vm.effectClasses.shrink = !vm.effectClasses.shrink;
+			}, 1000);
+		},
+		progressBarExpand: function () {
+			var vm = this;
+			var widthCur = 0;
+			var maxWidth = 200;
+			setInterval(function () {
+				vm.progressBar.width = widthCur < maxWidth ? widthCur + 'px' : maxWidth + 'px';
+				widthCur += 20;
+			}, 500);
+		},
+	},
 });
