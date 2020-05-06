0ceaee963b1f43dc3f56cbda05338941a4d90f85
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-05-06 18:16:59 +0300

    finish section 3

---
+++ section_3/app2.js
@@ -0,0 +1,10 @@
+new Vue({
+	el: '#app2',
+	data: {
+		ingredients: ['meat', 'fruit', 'cookies'],
+		persons: [
+			{ name: 'Max', age: 27, color: 'red' },
+			{ name: 'Anna', age: 'unknown', color: 'blue' },
+		],
+	},
+});
