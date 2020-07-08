8c5c75819cf9ac78915f93d9a69645369ffbfee2
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-08 03:35:48 +0300

    finish monster slayer project

--- Projects/Project 1 - Monster Brawler/app.js
+++ Projects/Project 1 - Monster Brawler/app.js
@@ -4,25 +4,65 @@ new Vue({
 		playerHealth: 100,
 		monsterHealth: 100,
 		gameIsRunning: false,
+		turns: [],
 	},
 	methods: {
 		startGame: function () {
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+			this.turns = [];
 		},
 		attack: function () {
-			this.monsterHealth -= this.calculateDamage(3, 10);
+			let damage = this.calculateDamage(3, 10);
+			this.monsterHealth -= damage;
+			this.turns.unshift({
+				isPlayer: true,
+				text: 'Player hits Monster for ' + damage,
+			});
 			if (this.checkWin()) {
 				return;
 			}
 
-			this.playerHealth -= this.calculateDamage(5, 12);
+			this.monsterAttacks();
+		},
+		specialAttack: function () {
+			let damage = this.calculateDamage(10, 20);
+			this.monsterHealth -= damage;
+			this.turns.unshift({
+				isPlayer: true,
+				text: 'Player hits Monster hard for ' + damage,
+			});
+			if (this.checkWin()) {
+				return;
+			}
+			this.monsterAttacks();
+		},
+		heal: function () {
+			if (this.playerHealth <= 90) {
+				this.playerHealth += 10;
+			} else {
+				playerHealth = 100;
+			}
+			this.turns.unshift({
+				isPlayer: true,
+				text: 'Player heals for 10',
+			});
+
+			this.monsterAttacks();
+		},
+		giveUp: function () {
+			this.gameIsRunning = false;
+		},
+		monsterAttacks: function () {
+			let damage = this.calculateDamage(5, 12);
+			this.playerHealth -= damage;
+			this.turns.unshift({
+				isPlayer: false,
+				text: 'Monster hits Player for ' + damage,
+			});
 			this.checkWin();
 		},
-		specialAttack: function () {},
-		heal: function () {},
-		giveUp: function () {},
 		calculateDamage: function (min, max) {
 			return Math.max(Math.floor(Math.random() * max) + 1, min);
 		},
