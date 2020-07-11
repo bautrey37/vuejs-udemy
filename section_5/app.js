f49e89a3ea2b29e6d5438fd2fb1a8fbaee1aa1ae
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-11 15:41:09 +0300

    section 5

---
+++ section_5/app.js
@@ -0,0 +1,57 @@
+var data = {
+	title: 'The VueJS Instance',
+	showParagraph: false,
+};
+
+var vm1 = new Vue({
+	data: data,
+	methods: {
+		show: function () {
+			this.showParagraph = true;
+			this.updateTitle('The VueJS Instance (Updated)');
+			this.$refs.myButton.innerText = 'Test';
+		},
+		updateTitle: function (title) {
+			this.title = title;
+		},
+	},
+	computed: {
+		lowercaseTitle: function () {
+			return this.title.toLowerCase();
+		},
+	},
+	watch: {
+		title: function (value) {
+			alert('Title changed, new value: ' + value);
+		},
+	},
+});
+
+vm1.$mount('#app1');
+
+console.log(vm1.$data === data);
+//vm1.$refs.heading.innerText = 'Something else';
+
+setTimeout(function () {
+	vm1.title = 'Changed by Timer';
+	vm1.show();
+}, 3000);
+
+var vm2 = new Vue({
+	el: '#app2',
+	data: {
+		title: 'The second Instance',
+	},
+	methods: {
+		onChange: function () {
+			vm1.title = 'Changed!';
+		},
+	},
+});
+
+var vm3 = new Vue({
+	template: '<h1>Hello!</h1>',
+});
+
+vm3.$mount();
+document.getElementById('app3').appendChild(vm3.$el);
