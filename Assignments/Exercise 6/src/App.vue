255b9272f0ee1b2c7d4f0304866aade3c12ee1e3
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-11 18:44:45 +0300

    personal formatter

--- Assignments/Exercise 6/src/App.vue
+++ Assignments/Exercise 6/src/App.vue
@@ -1,41 +1,34 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<header>
+					<h1>Server Status</h1>
+				</header>
+			</div>
+		</div>
+		<hr />
+		<div class="row">
+			<div class="col-xs-12 col-sm-6">
+				<ul class="list-group">
+					<li class="list-group-item" v-for="index in 5" :key="index">Server #{{ index }}</li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-6">
+				<p>Server Details are currently not updated</p>
+			</div>
+		</div>
+		<hr />
+		<div class="row">
+			<div class="col-xs-12">
+				<footer>
+					<p>All Servers are managed here</p>
+				</footer>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
-</script>
+<script></script>
 
-<style>
-
-</style>
+<style></style>
