252dae32fa9a84ce390db5f2fc54c88f7c3fc129
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-12 00:23:19 +0300

    props

--- section_8/src/components/User.vue
+++ section_8/src/components/User.vue
@@ -1,33 +1,44 @@
 <template>
-    <div class="component">
-        <h1>The User Component</h1>
-        <p>I'm an awesome User!</p>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
-            </div>
-        </div>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my Name</button>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail :name="name"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import UserDetail from './UserDetail.vue';
-    import UserEdit from './UserEdit.vue';
+import UserDetail from './UserDetail.vue';
+import UserEdit from './UserEdit.vue';
 
-    export default {
-        components: {
-            appUserDetail: UserDetail,
-            appUserEdit: UserEdit
-        }
-    }
+export default {
+  data: function () {
+    return {
+      name: 'brandon',
+    };
+  },
+  methods: {
+    changeName() {
+      this.name = 'max';
+    },
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit,
+  },
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightblue;
-    }
+div {
+  background-color: lightblue;
+}
 </style>
--- section_8/src/components/UserDetail.vue
+++ section_8/src/components/UserDetail.vue
@@ -1,15 +1,30 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-    </div>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+  </div>
 </template>
 
 <script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: 'autrey',
+    },
+  },
+  methods: {
+    switchName() {
+      return this.name.split('').reverse().join('');
+    },
+  },
+};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
