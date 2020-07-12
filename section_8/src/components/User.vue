0238048280e04c40311a2fa5bb32e8d9c3bc7cad
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-12 22:23:32 +0300

    section_8

--- section_8/src/components/User.vue
+++ section_8/src/components/User.vue
@@ -4,6 +4,7 @@
     <p>I'm an awesome User!</p>
     <button @click="changeName">Change my Name</button>
     <p>Name is {{ name }}</p>
+    <p>Age is {{ age }}</p>
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-6">
@@ -11,10 +12,15 @@
         <app-user-detail
           :name="name"
           @nameWasReset="name = $event"
+          :resetFn="resetName"
+          :userAge="age"
         ></app-user-detail>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <app-user-edit></app-user-edit>
+        <app-user-edit
+          :userAge="age"
+          @ageWasEdited="age = $event"
+        ></app-user-edit>
       </div>
     </div>
   </div>
@@ -28,11 +34,15 @@ export default {
   data: function () {
     return {
       name: 'brandon',
+      age: 28,
     };
   },
   methods: {
     changeName() {
       this.name = 'max';
+    },
+    resetName() {
+      this.name = 'brandon';
     },
   },
   components: {
--- section_8/src/components/UserDetail.vue
+++ section_8/src/components/UserDetail.vue
@@ -3,11 +3,16 @@
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
     <p>User Name: {{ switchName() }}</p>
+    <p>User Age: {{ userAge }}</p>
     <button @click="resetName">Reset Name</button>
+    <!-- calls a callback function which calls the function in the parent-->
+    <button @click="resetFn()">Reset Name</button>
   </div>
 </template>
 
 <script>
+import { eventBus } from '../main';
+
 export default {
   props: {
     name: {
@@ -15,6 +20,8 @@ export default {
       required: false,
       default: 'autrey',
     },
+    resetFn: Function,
+    userAge: Number,
   },
   methods: {
     switchName() {
@@ -24,6 +31,11 @@ export default {
       this.name = 'brandon';
       this.$emit('nameWasReset', this.name);
     },
+  },
+  created() {
+    eventBus.$on('ageWasEdited', (age) => {
+      this.userAge = age;
+    });
   },
 };
 </script>
