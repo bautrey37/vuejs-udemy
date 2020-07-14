041bcf811321835b951bc3832c9d6a44f60f881b
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-14 14:49:03 +0300

    Finish project 2

--- Projects/Project 2 - Wonderful Quotes/src/components/Quote.vue
+++ Projects/Project 2 - Wonderful Quotes/src/components/Quote.vue
@@ -1,13 +1,26 @@
 <template>
-  
+  <div class="col-sm-6 col-md-6 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
+<script></script>
 
 <style>
+.panel-body {
+  font-family: 'Arizonia', cursive;
+  font-size: 24pxcolor;
+  color: #6e6e6e;
+}
 
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: #ffe2e2;
+}
 </style>
--- Projects/Project 2 - Wonderful Quotes/src/components/QuoteGrid.vue
+++ Projects/Project 2 - Wonderful Quotes/src/components/QuoteGrid.vue
@@ -1,7 +1,29 @@
-<template> </template>
+<template>
+  <div class="row">
+    <quote
+      v-for="(quote, index) in quotes"
+      :key="quote"
+      @click.native="deleteQuote(index)"
+    >
+      {{ quote }}
+    </quote>
+  </div>
+</template>
 
 <script>
-export default {};
+import Quote from './Quote.vue';
+
+export default {
+  props: ['quotes'],
+  methods: {
+    deleteQuote(index) {
+      this.$emit('quoteDeleted', index);
+    },
+  },
+  components: {
+    Quote,
+  },
+};
 </script>
 
 <style></style>
