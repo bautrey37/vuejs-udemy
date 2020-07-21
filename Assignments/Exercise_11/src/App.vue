2312f247e2aa3183a5b23e2986254b3d9bab8e2f
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-21 17:00:21 +0300

    parts 1 & 2

--- Assignments/Exercise_11/src/App.vue
+++ Assignments/Exercise_11/src/App.vue
@@ -5,10 +5,14 @@
         <h1>Filters & Mixins</h1>
         <!-- Exercise 1) -->
         <!-- Build a local Filter which reverses the Text it is applied on -->
+        <input type="text" name="ex1" id v-model="ex1" />
+        <p>{{ ex1 | reverse }}</p>
 
         <!-- Exercise 2 -->
         <!-- Build a global Filter which counts the length of a word and it appends it -->
         <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+        <input type="text" name="ex2" id v-model="ex2" />
+        <p>{{ ex2 | count }}</p>
 
         <!-- Exercise 3 -->
         <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
@@ -21,7 +25,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      ex1: 'test',
+      ex2: 'example'
+    };
+  },
+  filters: {
+    reverse(value) {
+      return value
+        .split('')
+        .reverse()
+        .join('');
+    },
+    count(value) {
+      return `${value} (${value.length})`;
+    }
+  }
+};
 </script>
 
 <style></style>
