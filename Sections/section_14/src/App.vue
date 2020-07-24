d14d39818a9b16742176452ed11661e9643fd28d
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-24 11:35:12 +0300

    fade transition

--- Sections/section_14/src/App.vue
+++ Sections/section_14/src/App.vue
@@ -3,6 +3,14 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
+        <hr />
+        <button class="btn btn-primary" @click="show = !show">
+          Show Alert
+        </button>
+        <br /><br />
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -11,9 +19,26 @@
 <script>
 export default {
   data() {
-    return {};
-  }
+    return {
+      show: false,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+  /* opacity: 1 */
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+</style>
