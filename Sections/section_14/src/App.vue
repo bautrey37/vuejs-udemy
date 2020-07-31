610f7ef71e7d816cc77e68c5843cd6544eb2ae17
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-31 15:53:34 +0300

    finish transition

--- Sections/section_14/src/App.vue
+++ Sections/section_14/src/App.vue
@@ -4,21 +4,31 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr />
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br />
+        <br />
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br />
         <br />
-        <transition name="fade">
+        <transition :name="alertAnimation">
           <div class="alert alert-info" v-show="show">This is some info</div>
         </transition>
         <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+        <!-- Animate CSS animations -->
         <transition
-          appear
           enter-active-class="animate__animated animate__bounce"
           leave-active-class="animate__animated animate__shakeX"
         >
           <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+        <transition :name="alertAnimation" mode="out-in">
+          <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+          <div class="alert alert-warning" v-if="!show" key="warning">This is some Warning</div>
         </transition>
       </div>
     </div>
@@ -30,6 +40,7 @@ export default {
   data() {
     return {
       show: true,
+      alertAnimation: 'fade',
     };
   },
 };
