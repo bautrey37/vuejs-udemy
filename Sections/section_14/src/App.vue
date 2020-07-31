0d08dee68df272dadd1cfe4c4d0380690d6422ee
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-07-31 16:05:15 +0300

    javascript animation functions

--- Sections/section_14/src/App.vue
+++ Sections/section_14/src/App.vue
@@ -30,6 +30,22 @@
           <div class="alert alert-info" v-if="show" key="info">This is some info</div>
           <div class="alert alert-warning" v-if="!show" key="warning">This is some Warning</div>
         </transition>
+        <hr />
+        <button class="btn btn-primary" @click="load = !load">Load / Remove Element</button>
+        <br />
+        <br />
+        <transition
+          @before-enter="beforeEnter"
+          @enter="enter"
+          @after-enter="afterEnter"
+          @enter-cancelled="enterCancelled"
+          @before-leave="beforeLeave"
+          @leave="leave"
+          @after-leave="afterLeave"
+          @after-cancelled="leaveCancelled"
+        >
+          <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
+        </transition>
       </div>
     </div>
   </div>
@@ -40,8 +56,37 @@ export default {
   data() {
     return {
       show: true,
+      load: true,
       alertAnimation: 'fade',
     };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log('beforeEnter');
+    },
+    enter(el, done) {
+      console.log('enter');
+      done();
+    },
+    afterEnter(el) {
+      console.log('afterEnter');
+    },
+    enterCancelled(el) {
+      console.log('enterCancelled');
+    },
+    beforeLeave(el) {
+      console.log('beforeLeave');
+    },
+    leave(el, done) {
+      console.log('leave');
+      done();
+    },
+    afterLeave(el) {
+      console.log('afterLeave');
+    },
+    leaveCancelled(el) {
+      console.log('leaveCancelled');
+    },
   },
 };
 </script>
