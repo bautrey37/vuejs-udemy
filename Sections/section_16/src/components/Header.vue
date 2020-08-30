8aef401a79ce94cb2f979626f8f50e91f24db476
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-08-30 18:02:36 +0300

    header with bootstrap

---
+++ Sections/section_16/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <router-link to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/user">User</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
