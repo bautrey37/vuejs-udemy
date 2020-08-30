0af9ccf6ee4b0a2e92e2f73567e325f444351112
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-08-30 18:40:20 +0300

    children routes

--- Sections/section_16/src/components/Header.vue
+++ Sections/section_16/src/components/Header.vue
@@ -3,11 +3,8 @@
     <router-link to="/" tag="li" active-class="active" exact>
       <a>Home</a>
     </router-link>
-    <router-link to="/user/1" tag="li" active-class="active">
-      <a>User 1</a>
-    </router-link>
-    <router-link to="/user/2" tag="li" active-class="active">
-      <a>User 2</a>
+    <router-link to="/user" tag="li" active-class="active">
+      <a>User</a>
     </router-link>
   </ul>
 </template>
--- Sections/section_16/src/components/user/User.vue
+++ Sections/section_16/src/components/user/User.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
     <h1>The User Page</h1>
-    <p>Loaded ID: {{ id }}</p>
+    <hr>
     <button @click="navigateToHome" class="btn btn-primary">Go to home</button>
+    <hr>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-    };
-  },
-  // in version 2.2, you can also bind your route params to props of the target components. This eliminates the need to watching the route
-  watch: {
-    $route(to, from) {
-      this.id = to.params.id;
-    },
-  },
   methods: {
     navigateToHome() {
       this.$router.push('/');
