141aba50c46042002f035288c78d8e579a433757
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-08-30 18:42:43 +0300

    dynamic route linking

--- Sections/section_16/src/components/user/UserDetail.vue
+++ Sections/section_16/src/components/user/UserDetail.vue
@@ -2,5 +2,12 @@
   <div>
     <h3>Some User Details</h3>
     <p>User loaded has ID: {{ $route.params.id }}</p>
+    <router-link
+      tag="button"
+      :to="'/user/' + $route.params.id + '/edit'"
+      class="btn btn-primary"
+    >
+      Edit User
+    </router-link>
   </div>
 </template>
