06239c8b49e0cf0f92c426f7cd08f379cfc1815f
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-08-30 18:19:37 +0300

    current path

--- Sections/section_16/src/components/Header.vue
+++ Sections/section_16/src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <ul class="nav nav-pills">
-    <li class="nav-item">
-      <router-link to="/">Home</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/user">User</router-link>
-    </li>
+    <router-link to="/" tag="li" active-class="active" exact
+      ><a>Home</a></router-link
+    >
+    <router-link to="/user" tag="li" active-class="active"
+      ><a>User</a></router-link
+    >
   </ul>
 </template>
 
--- Sections/section_16/src/components/user/User.vue
+++ Sections/section_16/src/components/user/User.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>The User Page</h1>
+  <div>
+    <h1>The User Page</h1>
+    <button @click="navigateToHome" class="btn btn-primary">Go to home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome() {
+      this.$router.push('/');
+    },
+  },
+};
+</script>
