bdbd8e5271e180812666077ee96f7288b615da27
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-09-01 12:48:03 +0300

    animating header

--- Sections/section_16/src/App.vue
+++ Sections/section_16/src/App.vue
@@ -6,7 +6,9 @@
         <hr />
         <!-- <app-header></app-header> -->
         <router-view name="header-top"></router-view>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
         <router-view name="header-bottom"></router-view>
       </div>
     </div>
@@ -22,4 +24,37 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.slide-leave-action {
+  transition: opacity 1s ease;
+  opacity: 0;
+  animation: slide-out 1s ease-out forwards;
+}
+
+.slide-leave {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+}
+
+@keyframes slide-out {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-30px);
+  }
+}
+
+@keyframes slide-in {
+  0% {
+    transform: translateY(-30px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
