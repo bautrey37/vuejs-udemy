b48538cfa2de917c09f8b2db87e6b938bcea4887
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-09-01 13:31:21 +0300

    lazy routes; not sure if it works correctly

--- Sections/section_16/src/routes.js
+++ Sections/section_16/src/routes.js
@@ -1,9 +1,26 @@
-import User from './components/user/User.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
+
+const User = (resolve) => {
+  require.ensure(['./components/user/User.vue'], () => {
+    resolve(require('./components/user/User.vue'));
+  }, 'user');
+};
+const UserStart = (resolve) => {
+  require.ensure(['./components/user/UserStart.vue'], () => {
+    resolve(require('./components/user/UserStart.vue'));
+  }, 'user');
+};
+const UserEdit = (resolve) => {
+  require.ensure(['./components/user/UserEdit.vue'], () => {
+    resolve(require('./components/user/UserEdit.vue'));
+  }, 'user');
+};
+const UserDetail = (resolve) => {
+  require.ensure(['./components/user/UserDetail.vue'], () => {
+    resolve(require('./components/user/UserDetail.vue'));
+  }, 'user');
+};
 
 export const routes = [
   {
