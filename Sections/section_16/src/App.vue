1bc5cb8a6a566ffffa8c787eaf2a04fcfc75b6ba
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-09-01 12:36:41 +0300

    named router view

--- Sections/section_16/src/App.vue
+++ Sections/section_16/src/App.vue
@@ -4,19 +4,21 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <hr />
-        <app-header></app-header>
+        <!-- <app-header></app-header> -->
+        <router-view name="header-top"></router-view>
         <router-view></router-view>
+        <router-view name="header-bottom"></router-view>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
+// import Header from './components/Header.vue';
 export default {
-  components: {
-    appHeader: Header,
-  },
+  // components: {
+  //   appHeader: Header,
+  // },
 };
 </script>
 
