d9b4fbf51476a5ca0bde282c7e0bb26c6e1da67b
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-09-04 23:09:43 +0300

    implement Stock component

--- Projects/Project 3 - Stock Trader/stock-trader/src/components/stocks/Stock.vue
+++ Projects/Project 3 - Stock Trader/stock-trader/src/components/stocks/Stock.vue
@@ -1,11 +1,56 @@
 <template>
-  <div>
-    <h1>Stocks - Stock component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card bg-success">
+      <div class="card-header">
+        <h3 class="card-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </h3>
+      </div>
+      <div class="card-body bg-light">
+        <div class="float-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Quantity"
+            v-model.number="quantity"
+          />
+        </div>
+        <div class="float-right">
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="quantity <= 0"
+          >
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+      console.log(order);
+      this.quantity = 0;
+    },
+    disableButton() {}
+  }
+};
 </script>
 
 <style></style>
--- Projects/Project 3 - Stock Trader/stock-trader/src/views/Stocks.vue
+++ Projects/Project 3 - Stock Trader/stock-trader/src/views/Stocks.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <h1>Stocks component</h1>
+    <app-stock
+      v-for="stock in stocks"
+      :key="stock.id"
+      :stock="stock"
+    ></app-stock>
   </div>
 </template>
 
 <script>
-export default {};
+import Stock from '../components/stocks/Stock.vue';
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110 },
+        { id: 2, name: 'Google', price: 250 },
+        { id: 3, name: 'Apple', price: 400 },
+        { id: 4, name: 'Twitter', price: 50 }
+      ]
+    };
+  },
+  components: {
+    appStock: Stock
+  }
+};
 </script>
 
 <style></style>
